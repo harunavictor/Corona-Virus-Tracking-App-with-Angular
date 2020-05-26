e50f1da048d4610a2648f3c13c43e1def2803545
victor <harunavictor754@gmail.com>
2020-05-26 20:55:02 +0100

    corona app with angular9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CoronaService } from "./services/corona.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'angularcorona';
+export class AppComponent implements OnInit {
+  countries: any[];
+  country: any;
+  confirmed: number
+  recovered: number
+  deaths: number
+  
+  constructor(private coronaServ: CoronaService) {}
+  ngOnInit() {
+    this.coronaServ.getCountries().subscribe((response) => {
+      // console.log(response);
+      this.countries = response;
+    });
+  }
+  getCoronaData() {
+    this.coronaServ.getCoronaRealTimeData(this.country).subscribe((data) => {
+      console.log(data) 
+      const index = data.length - 1
+      console.log(index)
+      this.confirmed = data[index].Confirmed
+      this.recovered = data[index].Recovered
+      this.deaths = data[index].Deaths
+      
+    })
+  }
+  getCountry(country: any) {
+    this.country = country;
+  }
 }
